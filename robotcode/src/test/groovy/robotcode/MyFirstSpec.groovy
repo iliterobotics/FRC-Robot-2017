97fbfc6077c43ef10f12b43d5510e4ef906a5108
Chris <coconnell84@gmail.com>
2017-01-28 12:59:06 -0500

    Added new unit test

--- robotcode/src/test/groovy/robotcode/MyFirstSpec.groovy
+++ robotcode/src/test/groovy/robotcode/MyFirstSpec.groovy
@@ -1,9 +1,19 @@
+package robotcode
 import spock.lang.*
+
+
 
 // Hit 'Run Script' below
 class MyFirstSpec extends Specification {
   def "let's try this!"() {
     expect:
     Math.max(1, 2) == 2
+  }
+  
+  def "THis will break. "() { 
+	  
+	  expect:
+	  Math.max(1,2) ==2
+	 
   }
 }
