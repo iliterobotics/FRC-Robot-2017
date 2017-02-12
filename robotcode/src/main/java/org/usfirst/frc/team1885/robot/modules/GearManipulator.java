cac61b4c904ce6d517a319bac9c4eb2bb0b9f16c
Michael Kelly <rockymkelly@gmail.com>
2017-02-12 09:17:30 -0500

    Added gear manipulator module and autonomous command to drop off gear

--- robotcode/src/main/java/org/usfirst/frc/team1885/robot/modules/GearManipulator.java
+++ robotcode/src/main/java/org/usfirst/frc/team1885/robot/modules/GearManipulator.java
@@ -1,73 +1,60 @@
 package org.usfirst.frc.team1885.robot.modules;
 
-import java.util.HashMap;
 import java.util.Map;
 
-import org.usfirst.frc.team1885.robot.modules.driverControl.DriverControl;
-import org.usfirst.frc.team1885.robot.modules.driverControl.DriverControl.ControllerType;
-
-import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class GearManipulator implements Module{
-	private static final int POSITION_1 = 0;
-	private static final int POSITION_2 = 65;
-	private static final int GEAR_TOGGLE = 5;
-	private Map<ServoType, Servo> servoMap = new HashMap<ServoType, Servo>();
-	private boolean toggle;
-	private DriverControl driverControl;
-	private boolean previousState;
+
+	private Map<PistonType, Solenoid> pistonMap;
 	
-	private enum ServoType {
-		RIGHT_GEAR_HOLD(0), LEFT_GEAR_HOLD(1);
+	private boolean isOpen;
+	private boolean isTilted;
+	private boolean isKeked;
+	
+	private enum PistonType {
+		DOOR_DOWN(0), DOOR_TIPPER(1), KICKER(2);
 		
-		final int channel;
-		int position;
+		public final int port;
 		
-		ServoType(int channel) {
-			this.channel = channel;
-			position = POSITION_1;
+		PistonType(int port){
+			this.port = port;
 		}
 	}
 	
-	public GearManipulator(DriverControl driverControl) {
-		servoMap = new HashMap<>();
-		this.driverControl = driverControl;
-		toggle = previousState = false;
-	}
+	public GearManipulator(){
+	}	
 
 	@Override
 	public void initialize() {
-		for(ServoType type : ServoType.values()) {
-			Servo servo = new Servo(type.channel);
-			servoMap.put(type, servo);
-		}		
+		for(PistonType type : PistonType.values()) {
+			Solenoid solenoid = new Solenoid(type.port);
+			pistonMap.put(type, solenoid);
+		}
+	}
+	
+	public void setOpen(boolean open){
+		this.isOpen = open;
+	}
+	
+	public void setTilted(boolean tilted){
+		this.isTilted = tilted;
+	}
+	
+	public void setKick(boolean kicked){
+		if(!isOpen && kicked){
+			kicked = false;
+		}
+		isKeked = kicked;
 	}
 
 	@Override
 	public void update() {
-		if(driverControl.getController(ControllerType.CONTROLLER).getRawButton(GEAR_TOGGLE) && !previousState) {
-			previousState = toggle;
-			toggle = !toggle;
-		}
-		else if(!driverControl.getController(ControllerType.CONTROLLER).getRawButton(GEAR_TOGGLE)){
-			previousState = false;
-		}
-		if(toggle) {
-			servoMap.get(servoMap.get(ServoType.LEFT_GEAR_HOLD)).set(0.0);
-			servoMap.get(servoMap.get(ServoType.RIGHT_GEAR_HOLD)).set(0.0);
-		}
-		else {
-			servoMap.get(servoMap.get(ServoType.LEFT_GEAR_HOLD)).set(0.3);
-			servoMap.get(servoMap.get(ServoType.RIGHT_GEAR_HOLD)).set(0.5);
-		}
-//		if(driverControl.getController(ControllerType.CONTROLLER).getRawButton(5)) {
-//			servoMap.get(ServoType.LEFT_GEAR_HOLD).set(0.0);
-//			servoMap.get(ServoType.RIGHT_GEAR_HOLD).set(0.0);
-//		}
-//		else if(driverControl.getController(ControllerType.CONTROLLER).getRawButton(6)) {
-//			servoMap.get(ServoType.LEFT_GEAR_HOLD).set(0.5);
-//			servoMap.get(ServoType.RIGHT_GEAR_HOLD).set(0.5);
-//		}
+		pistonMap.get(PistonType.DOOR_DOWN).set(isOpen);
+		pistonMap.get(PistonType.DOOR_TIPPER).set(isTilted);
+		pistonMap.get(PistonType.KICKER).set(isKeked);			
 	}
 	
 }
