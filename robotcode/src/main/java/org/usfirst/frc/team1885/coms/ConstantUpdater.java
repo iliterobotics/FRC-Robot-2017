76eefb61c3a563500153507df1390eeb439321cb
Michael Kelly <rockymkelly@gmail.com>
2017-02-28 11:52:11 -0500

    Added networktables offloading and generic update queuing

--- robotcode/src/main/java/org/usfirst/frc/team1885/coms/ConstantUpdater.java
+++ robotcode/src/main/java/org/usfirst/frc/team1885/coms/ConstantUpdater.java
@@ -9,16 +9,24 @@ import java.net.URL;
 import java.util.LinkedList;
 import java.util.Queue;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class ConstantUpdater implements Runnable {
+	
+	public static final String ADDRESS = "http://michael-laptop.local";
+	public static final String NETWORK_TABLE = "Live values";
+	
 	public static ConstantUpdater instance;
+	public static Thread instanceThread;
+	
 	private boolean running;
-	private Queue<Update> update;
-	private Update currentUpdate;
+	private Queue<Update<?>> updateQueue;
+	
+	private NetworkTable netTable;
 	
 	private ConstantUpdater() {
-		update = new LinkedList<Update>();
+		updateQueue = new LinkedList<>();
+		netTable = NetworkTable.getTable(NETWORK_TABLE);
 		running = true;
 	}
 	
@@ -31,47 +39,93 @@ public class ConstantUpdater implements Runnable {
 	
 	public void run() {
 		while ( running ) {
-			while (update.peek() != null ) {
-				currentUpdate = update.poll();
-				String url = ConstantGetter.ADDRESS + "/constant/" + currentUpdate.variable + "/" + currentUpdate.value;
-				try {
-					URL obj = new URL(url);
-					HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-					con.setRequestMethod("GET");
-					int responseCode = con.getResponseCode();
-				BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-				String inputLine;
-				StringBuffer response = new StringBuffer();
-				while((inputLine = in.readLine()) != null){
-					response.append(inputLine);
-				}
-				in.close();
-				} 
-				catch (MalformedURLException e) {} 
-				catch (IOException e) {}
+			while (updateQueue.peek() != null ) {
+				Update<?> currentUpdate = updateQueue.poll();
+				pushToNetworkTables(currentUpdate);
+				pushToWebserver(currentUpdate);
 			}
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
-				
+				running = false;
 			}	
 		}
 	}
 	
-	public void stop(){
-		running = false;
+	private void pushToWebserver(Update<?> currentUpdate){
+		String url = ConstantGetter.ADDRESS + "/constant/" + currentUpdate.variable + "/" + currentUpdate.value;
+		try {
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			con.setRequestMethod("GET");
+			//int responseCode = con.getResponseCode();
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while((inputLine = in.readLine()) != null){
+				response.append(inputLine);
+			}
+			in.close();
+		} 
+		catch (MalformedURLException e) {} 
+		catch (IOException e) {}		
 	}
 	
-	public void addToConstant( String var, String val ){
-		update.add(new Update(var, val));
+	private void pushToNetworkTables(Update<?> currentUpdate){
+		if(currentUpdate.value instanceof Number){
+			double doubleValue = ((Number)(currentUpdate.value)).doubleValue();
+			netTable.putNumber(currentUpdate.variable, doubleValue);
+		}
+		else if(currentUpdate.value instanceof Number[]){
+			Number[] numberArrayValue = ((Number [])(currentUpdate.value));
+			double[] doubleArrayValue = new double[numberArrayValue.length];
+			for(int i = 0; i < numberArrayValue.length; i++){
+				doubleArrayValue[i] = numberArrayValue[i].doubleValue();
+			}
+			netTable.putNumberArray(currentUpdate.variable, doubleArrayValue);
+		}
+		else{ //Treat it as a string
+			String stringValue = (currentUpdate.value.toString());
+			netTable.putString(currentUpdate.variable, stringValue);
+		}
 	}
 	
-	private class Update{
-		public String variable, value;
-		Update(String var, String val) {
+	public static void restart(){
+		if(instance != null) instance.running = false;
+		instance = null;
+	}
+	
+	public static void start(){
+		if(instanceThread == null){
+			instanceThread = new Thread(getInstance());
+		} else {
+			restart();
+		}
+	}
+	
+	public static void stop(){
+		getInstance().running = false;
+	}
+	
+	public static void putString( String var, String val ){
+		getInstance().updateQueue.add(new Update<String>(var, val));
+	}
+	
+	public static void putNumber(String var, Number val){
+		getInstance().updateQueue.add(new Update<Number>(var, val));		
+	}
+	
+	public static void putNumberArray(String var, Number[] vals){
+		getInstance().updateQueue.add(new Update<Number[]>(var, vals));
+	}
+	
+	private static class Update<E extends Object>{
+		public String variable;
+		public E value;
+		Update(String var, E val) {
 			this.variable = var;
 			this.value = val;
 		}
-	}
+	}	
 	
 }
